5d05d4f17417e797c5ac126aea2006fda65de6e7
thomasum <jeayagosu@gmail.com>
2024-11-26 11:58:45 +0900

    Commit messsage

--- app/src/main/java/com/example/test/Frag3.java
+++ app/src/main/java/com/example/test/Frag3.java
@@ -1,9 +1,12 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +21,78 @@ public class Frag3 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag3, container, false);
+
+        ImageButton rating1_1Button = view.findViewById(R.id.rating1_1);
+        rating1_1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating1_1.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating1_2Button = view.findViewById(R.id.rating1_2);
+        rating1_2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating1_2.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating2_1Button = view.findViewById(R.id.rating2_1);
+        rating2_1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating2_1.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating2_2Button = view.findViewById(R.id.rating2_2);
+        rating2_2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating2_2.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating3_1Button = view.findViewById(R.id.rating3_1);
+        rating3_1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating3_1.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating3_2Button = view.findViewById(R.id.rating3_2);
+        rating3_2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating3_2.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating4_1Button = view.findViewById(R.id.rating4_1);
+        rating4_1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating4_1.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton rating4_2Button = view.findViewById(R.id.rating4_2);
+        rating4_2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Rating4_2.class);
+                startActivity(intent);
+            }
+        });
 
 
         return view;
---
+++ app/src/main/java/com/example/test/Rating4_1.java
@@ -0,0 +1,39 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Rating4_1 extends AppCompatActivity {
+
+    TextView tvScore;
+    Button btnSubmit;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rating4_1);
+
+        initView();
+    }
+
+    private void initView() {
+        btnSubmit = findViewById(R.id.button);
+        tvScore = findViewById(R.id.tv_score);
+        ratingBar = findViewById(R.id.ratings);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float rating = ratingBar.getRating();
+                tvScore.setText(String.valueOf("평점 : " + rating));
+            }
+        });
+
+    }
+}
